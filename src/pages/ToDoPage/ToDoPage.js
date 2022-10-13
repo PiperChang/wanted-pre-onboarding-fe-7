272d19d34bc7bfe6522d1358b99024c10e6e32b3
KCHamp <goddhqj8000@ajou.ac.kr>
2022-10-13 19:22:18 +0900

    [#12] Feat: To Do List 컴포넌트 구현
    - TodoList, ToDoPage로 컴포넌트 나뉘어서 구현
    - getTodos 이용해 구현
    
    Related to: #12

--- src/pages/ToDoPage/ToDoPage.js
+++ src/pages/ToDoPage/ToDoPage.js
@@ -1,5 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getTodos } from '../../api/todo'
+
+const TodoList = ({ todos }) => {
+    const handleClickCompleteBtn = (e) => {
+        // e.target.
+    }
+    const handleClickDeleteBtn = (e) => {}
+    const handleClickUpdateBtn = (e) => {}
+    return (
+        <ul>
+            <h1>To Do List</h1>
+            {todos.map((todo) => {
+                return (
+                    <li key={todo.id}>
+                        <input
+                            type="checkbox"
+                            onClick={handleClickCompleteBtn}
+                        />
+                        <button
+                            id="삭제"
+                            onClick={handleClickDeleteBtn}
+                        ></button>
+                        <button
+                            id="수정"
+                            onClick={handleClickUpdateBtn}
+                        ></button>
+                        {todo.todo}
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
 
 export default function ToDoPage() {
-    return <div>ToDoPage</div>
+    const [todoList, setTodoList] = useState([])
+
+    useEffect(() => {
+        async function getTodosData() {
+            const todos = await getTodos()
+            setTodoList(todos.data)
+        }
+        getTodosData()
+    }, [])
+
+    return (
+        <div>
+            <TodoList todos={todoList} />
+        </div>
+    )
 }
