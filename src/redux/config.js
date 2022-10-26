a69f149e90e87c2c1169a5c2d6576d5a8891255b
김창희 <45018724+PiperChang@users.noreply.github.com>
2022-10-26 19:33:41 +0900

    Fix: Redux 사용 금지

--- src/redux/config.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { useSelector, useDispatch } from 'react-redux/es/exports'
-import { rootReducer } from '.'
-
-console.log(rootReducer)
-const store = configureStore({ reducer: rootReducer })
-
-export default store
